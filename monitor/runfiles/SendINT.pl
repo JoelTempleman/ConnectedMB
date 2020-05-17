7fd92287c4a6a390b5d74d44e6aa46d9fba55af8
Joel Templeman <root@localhost.localdomain>
2020-05-17 10:50:22 -0500

    added auto IP detection

--- monitor/runfiles/SendINT.pl
+++ monitor/runfiles/SendINT.pl
@@ -5,12 +5,46 @@ use warnings;
 
 use 5.010;
 
+sub GetServerAddress {
+
+    use File::Spec;
+
+    if ( not "/tmp/runfiles/" ) {
+       die qq(Directory doesn't exist);
+    }
+
+    my $filePath = "/tmp/runfiles/serverlist";
+
+    open( my $fileHandle, "<", $filePath )
+       or die qq(Cannot open $filePath for reading! $!);  # 1.
+
+    my $test_sequence_value;                              # 2.
+    while( my $line = <$fileHandle> ) {
+        chomp $line;
+        next unless $line =~ /^\s*DBASE\s*=\s*(.*)\s*/; # 3.
+        $test_sequence_value = $1;
+        last;
+    }
+    close $fileHandle;
+    if ( defined $test_sequence_value ) {                 # 4.
+        # Whatever you do if you find that value...
+        return $test_sequence_value;
+    }
+    else {
+        # Whatever you do if the value isn't found...
+        return;
+    }
+}
+
+my $IP = GetServerAddress();
+#print $IP;
+
 use Net::Ping;
 my $p = Net::Ping->new();
-if ($p->ping('172.16.3.228')) {
-       say 'Influx Sever Online';
+if ($p->ping( $IP )) {
+       say "Influx Server Online & $IP";
     } else {
-   say 'Influx Server Offine';
+   say "Influx Server Offine & $IP";
 exit;
 }
 
@@ -37,7 +71,7 @@ print "$name ";
  `/tmp/runfiles/tmp2/./awk-int -f $name.csv`;   
 #$names=${name%-2*};
 #print "$names";    
-      `/tmp/runfiles/CSV/./csv-to-influxdb --batch-size=5000 --server=http://172.16.3.228:8086 --database=$mac --measurement=$name /tmp/runfiles/CSV-INT/$name.csv `;
+      `/tmp/runfiles/CSV/./csv-to-influxdb --batch-size=5000 --server=http://$IP:8086 --database=$mac --measurement=$name /tmp/runfiles/CSV-INT/$name.csv `;
   }
 }
 print "\ndone :)";
