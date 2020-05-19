5b9b415bad122b79438b57b4a9c5355f41ac4d68
Joel Templeman <root@localhost.localdomain>
2020-05-18 19:17:49 -0500

    path changes

--- monitor/runfiles/Sendinflux.pl
+++ monitor/runfiles/Sendinflux.pl
@@ -38,16 +38,16 @@ sub GetServerAddress {
 my $IP = GetServerAddress();
 #print $IP;
 
-now="$(DATE)"
+now=$(date)
 
 use Net::Ping;
 my $p = Net::Ping->new();
 if ($p->ping( $IP )) {
        say "Influx Server Online & $IP";
-       echo "$now Influx Server Online & $IP" > /debug/logfile;
+       echo "$now - Influx Server Online & $IP" > /debug/logfile;
     } else {
    say "Influx Server Offine & $IP";
-   echo "$now Influx Server OFFLINE & $IP" > /debug/logfile;
+   echo "$now - Influx Server OFFLINE & $IP" > /debug/logfile;
 exit;
 }
 
