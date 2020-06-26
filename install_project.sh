5ee2f881cadd838b5c9bbd70226f782b07bd169c
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2020-06-26 10:12:19 -0500

    Update install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -3,8 +3,13 @@
 # Script used to install Git Hub repo into the correct areas on the server
 # Name: "install_project.sh" Copyright (c) Joel Templeman 2020
 
-cp -rf /home/ConnectedMB/server/serverfiles/* /home.ConnectedMB/
+# There should already be a folder called /home/connectin that was created when the root OS was installed. Note: This name is used
+# simply because the source project this was forked from was called "connectin" and the code uses this user heavily in the code.
 
-mkdir /var/www/html/packages/
+cp -rf /ConnectedMB/server/serverfiles/* /home/connectin/ # This will contain a clone copy of the Git Hub project
 
-cp -rf /home/ConnectedMB/monitor/* /var/www/html/packages/
+mkdir /var/www/ # This directory will be used by the web server. 
+mkdir /var/www/html/ # This directory will be used by the web server.
+mkdir /var/www/html/packages/ # This directory will be used by the web server.
+
+cp -rf /ConnectedMB/monitor/* /var/www/html/packages/ # Later, the client will use the wget command to download all these files.
