06109ebc9ef7409e16bbd70135dbce740b164572
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2020-06-26 09:04:03 -0500

    Update install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -1,2 +1,10 @@
-# Used to install Git Hub repo into the correct areas
+#!/bin/sh
 
+# Script used to install Git Hub repo into the correct areas on the server
+# Name: "install_project.sh" Copyright (c) Joel Templeman 2020
+
+cp -rf /home/ConnectedMB/server/serverfiles/* /home.ConnectedMB/
+
+mkdir /var/www/html/packages/
+
+cp -rf /home/ConnectedMB/monitor/* /var/www/html/packages/
